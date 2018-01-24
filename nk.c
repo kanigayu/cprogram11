fb24f1b7e1df20ba1f09feac0f2f35c5080b0b0f
kanigayu <31438605+kanigayu@users.noreply.github.com>
2018-01-24 15:38:30 +0530

    nk.c
    nk

--- nk.c
+++ nk.c
@@ -1,7 +1,11 @@
 #include<stdio.h>
 int main()
 {
-int N[size],size=2,k[i],i,sum=0;
-scanf("%d",&N[size]);
+int N[size1],N[size2],k[i],i,sum=0;
+scanf("%d",&N[size1],&N[size2]);
 scanf("%d",&k[i]);
+  sum=N[size1]+N[size2];
+  printf("%d",sum);
+  return 0;
+}
 
