6b0b3a6ddee51389f5f64627199f5a254d2d2bf7
kanigayu <31438605+kanigayu@users.noreply.github.com>
2018-01-24 16:24:36 +0530

    nk.c
    nk

--- nk.c
+++ nk.c
@@ -4,12 +4,10 @@ int main()
 int n[7]={7,6,5,4,3,2,1},i,sum=0,k;
   printf("Enter the value of k");
   scanf("%d",&k);
-  for(i=o;i<k;i++)
+  for(i=0;i<k;i++)
   {
     sum=sum+n[i];
   }
   printf("The num is %d",sum);
-}
 return 0;
 }
-
